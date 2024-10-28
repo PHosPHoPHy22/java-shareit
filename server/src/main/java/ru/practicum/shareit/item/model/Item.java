0234935e5f75172142440e84883cb033e6f4ac8b
Ksenia <kseniiabelka05@mail.ru>
2024-10-28 20:16:20 +0300

    тз 16

--- server/src/main/java/ru/practicum/shareit/item/model/Item.java
+++ server/src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -16,6 +16,7 @@ public class Item {
     private Long id;
     private String name;
     private String description;
+
     private Boolean available;
     @JoinColumn(name = "owner_id")
     @ManyToOne(fetch = FetchType.LAZY)
